6f828529223f850239244a3eee03014b183f6e61
Florian Klein <fklein@users.noreply.github.com>
2025-04-11 20:00:13 +0200

    Disable font ligatures in kitty and wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ config.color_scheme = "Catppuccin Macchiato"
 config.font = wezterm.font("FiraCode Nerd Font", {weight = "Medium"})
 -- config.font = wezterm.font("JetBrains Mono")
 config.font_size = 10.0
+config.harfbuzz_features = { 'calt=0' }
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
