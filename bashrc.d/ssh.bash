3a33b5bb70ae4a4942c6cad64c5d1d7c24bfb3d2
Florian Klein <fklein@users.noreply.github.com>
2026-01-30 21:48:25 +0100

    Update SSH agent setup

--- bashrc.d/ssh.bash
+++ bashrc.d/ssh.bash
@@ -1,21 +1,16 @@
-SSH_ENV="$HOME/.ssh/agent-environment"
+SSH_ENV="${HOME}/.ssh/agent-environment"
 
-function start_ssh_agent {
+start_ssh_agent() {
     echo "Initialising new SSH agent ..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
+    /usr/bin/ssh-agent | sed 's/^echo/# echo/' >"${SSH_ENV}"
     chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" >/dev/null
+    source "${SSH_ENV}" >/dev/null
     /usr/bin/ssh-add
 }
 
-# Source SSH settings, if applicable
-
-if [[ -f "${SSH_ENV}" ]]; then
-    . "${SSH_ENV}" >/dev/null
-    # ps $SSH_AGENT_PID doesn't work under Cygwin
-    ps -ef | grep "${SSH_AGENT_PID}" | grep "ssh-agent$" >/dev/null || {
-        start_ssh_agent
-    }
+if [[ -f ${SSH_ENV} ]]; then
+    source "${SSH_ENV}" >/dev/null
+    [[ -S ${SSH_AUTH_SOCK} ]] || start_ssh_agent
 else
     start_ssh_agent
 fi
