b83af798abbfe9f9408d85e137865b4e5c4e0678
Florian Klein <fklein@users.noreply.github.com>
2026-02-04 21:50:59 +0100

    Fix Starship not working with WezTerm

--- bashrc.d/starship.bash
+++ bashrc.d/starship.bash
@@ -1,3 +1,4 @@
 if command -v starship >/dev/null 2>&1; then
-    alias star='eval "$(starship init bash)"'
+    # See: https://github.com/wezterm/wezterm/issues/6776
+    alias star='eval "$(starship init bash)" && command -v __bp_install >/dev/null 2>&1 && __bp_install'
 fi
