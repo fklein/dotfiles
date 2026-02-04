336e44af6cb0f2b72f28147471160b7bd970da2f
Florian Klein <fklein@users.noreply.github.com>
2026-02-04 21:53:54 +0100

    Use standard weight font for WezTerm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Macchiato"
---config.font = wezterm.font("FiraCode Nerd Font")
-config.font = wezterm.font("FiraCode Nerd Font", {weight = "Medium"})
+config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font("FiraCode Nerd Font", {weight = "Medium"})
 -- config.font = wezterm.font("JetBrains Mono")
 config.font_size = 10.0
 config.harfbuzz_features = { 'calt=0' }
