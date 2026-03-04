6d59e14a55aa7f4d767a1dfb5db6e45186c5569f
Florian Klein <fklein@users.noreply.github.com>
2026-03-04 09:38:15 +0100

    Add bash function for navigating directory levels

--- bashrc.d/global.bash
+++ bashrc.d/global.bash
@@ -117,6 +117,29 @@ complete -o nospace -F _cdhistory cdhistory
 alias cd='cdhistory'
 complete -o nospace -F _cdhistory cd
 
+# Function to go up N directory levels
+up() {
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number" >&2
+        return 1
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive" >&2
+        return 2
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
 
 # Colorize text with terminal escape sequences
 # Usage: colorize [format,...] [format,...] -- [text ...]
